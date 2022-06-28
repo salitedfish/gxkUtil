b53bc753a6c86a21412ec4f368cdae824687282b
salitedfish <1525185228@qq.com>
2022-06-28 09:40:21 +0800

    update

---
+++ src/util/npm/index.ts
@@ -0,0 +1,7 @@
+/**专门用来导出第三方库 */
+import SparkMD5 from "spark-md5";
+import CryptoJS from "crypto-js";
+import axios from "axios";
+import Clipboard from "clipboard";
+
+export { SparkMD5, CryptoJS, axios, Clipboard };
