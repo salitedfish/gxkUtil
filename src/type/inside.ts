56e777ee3e054f1a1cfb2e64453a184704753e5b
salitedfish <1525185228@qq.com>
2022-07-28 17:32:53 +0800

    chore: 添加ts内置类型源码

--- src/type/inside.ts
+++ src/type/inside.ts
@@ -2,31 +2,57 @@ import { Params } from "./index";
 /**
  * ts内置工具类型
  */
-
-/**Omit高级类型 */
-
-/**Pick高级类型 */
-
-/**Record高级类型 */
-
-/**Exclude高级类型 */
-
-/**Extract高级类型 */
-
-/**ReturnType高级类型 */
-
-/**Partial高级类型 */
-
-/**Required高级类型 */
-
-/**Readonly高级类型 */
-
-/**Readonly高级类型 */
-
-/**Readonly高级类型 */
-
 /**Parameters高级类型 */
 export type _Parameters<F extends Function> = F extends (...args: infer P) => any ? P : never;
 
-/**Parameters高级类型 */
+/**ReturnType高级类型 */
 export type _ReturnType<F extends Function> = F extends (...args: Params) => infer R ? R : never;
+
+/**Omit高级类型 */
+export type _Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+/**Pick高级类型 */
+export type _Pick<T, P extends keyof T> = {
+  [K in P]: T[P];
+};
+
+/**Record高级类型 */
+export type _Record<P extends keyof any, T> = {
+  [K in P]: T;
+};
+
+/**Exclude高级类型 */
+export type _Exclude<T, U> = T extends U ? never : T;
+
+/**Extract高级类型 */
+export type _Extract<T, U> = T extends U ? T : never;
+
+/**Partial高级类型 */
+export type _Partial<T> = {
+  [K in keyof T]?: T[K];
+};
+
+/**Required高级类型 */
+export type _Required<T> = {
+  [K in keyof T]-?: T[K];
+};
+
+/**Readonly高级类型 */
+export type _Readonly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+/**NonNullable高级类型 */
+export type _NonNullable<T> = T extends null | undefined ? never : T;
+
+/**ConstructorParameters */
+export type _ConstructorParameters<T extends abstract new (...args: any) => any> = T extends abstract new (...args: infer P) => any ? P : never;
+
+/**InstanceType */
+export type _InstanceType<T extends abstract new (...args: any) => any> = T extends abstract new (...args: any) => infer R ? R : any;
+
+/**ThisParameterType */
+export type _ThisParameterType<T> = T extends (this: infer U, ...args: any[]) => any ? U : unknown;
+
+/**OmitThisParameter */
+export type _OmitThisParameter<T> = unknown extends ThisParameterType<T> ? T : T extends (...args: infer A) => infer R ? (...args: A) => R : T;
