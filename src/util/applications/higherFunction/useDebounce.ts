efa8acf5aa071d1b90ce3521aa99b159357ae805
salitedfish <1525185228@qq.com>
2022-10-08 11:53:30 +0800

    refactor: 优化类型

--- src/util/applications/higherFunction/useDebounce.ts
+++ src/util/applications/higherFunction/useDebounce.ts
@@ -1,11 +1,10 @@
-type UseDebounce = <V extends any[], R>(callBack: (...params: V) => R, countDown?: number) => (...params: V) => R | void;
 /**
  * 防抖Hook，多次操作，也只有最后一次触发回调
  * @param callBack
  * @param countDown 默认为1000
  * @returns
  */
-export const useDebounce: UseDebounce = (callBack, countDown = 1000) => {
+export const useDebounce = <V extends any[], R>(callBack: (...params: V) => R, countDown: number = 1000): ((...params: V) => R | void) => {
   let timer: NodeJS.Timeout;
   return (...params) => {
     if (timer) {
--- src/util/applications/higherFunction/useThrottle.ts
+++ src/util/applications/higherFunction/useThrottle.ts
@@ -1,11 +1,10 @@
-type UseThrottle = <V extends any[], R>(callBack: (...params: V) => R, countDown?: number) => (...params: V) => R | void;
 /**
  * 节流Hook，即使多次操作，一定时间内只能触发一次回调
  * @param callBack
  * @param countDown 默认为1000
  * @returns
  */
-export const useThrottle: UseThrottle = (callBack, countDown = 1000) => {
+export const useThrottle = <V extends any[], R>(callBack: (...params: V) => R, countDown: number = 1000): ((...params: V) => R | void) => {
   let lock = false;
   return (...params) => {
     if (!lock) {
