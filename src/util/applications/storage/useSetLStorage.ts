0c6d488ec4b94b1bc4a0c1f7ed51896910bdae6c
salitedfish <1525185228@qq.com>
2022-11-17 10:09:37 +0800

    fix: 修复useSetLStorage bug

--- src/util/applications/storage/useSetLStorage.ts
+++ src/util/applications/storage/useSetLStorage.ts
@@ -6,7 +6,12 @@ import { useCurryTwo } from "../../../util/currying";
  * @param value 数组对象自动转化为json
  */
 const useSetLStorageShallow = (key: string, value: unknown) => {
-  const valueJSON = JSON.stringify(value);
+  let valueJSON: string;
+  if (typeof value !== "string") {
+    valueJSON = JSON.stringify(value);
+  } else {
+    valueJSON = value;
+  }
   localStorage.setItem(key, valueJSON);
 };
 export const useSetLStorage = useCurryTwo(useSetLStorageShallow);
