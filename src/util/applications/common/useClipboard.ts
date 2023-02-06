d4d6afb01e307615a199a69ff0a345b1f92a1885
codergxk <1525185228@qq.com>
2023-02-06 15:21:52 +0800

    feat: 拷贝使用第三方库

--- src/util/applications/common/useClipboard.ts
+++ src/util/applications/common/useClipboard.ts
@@ -1,19 +1,23 @@
-import { useConsoleWarn } from "../../../useInside";
+import ClipboardJS from "clipboard";
 
 /**
  * 点击复制
  * @param text 复制的文本
  */
-export const useClipboard = async (text: string | number) => {
-  if (!navigator.clipboard) {
-    useConsoleWarn("useClipboard: 你的浏览器当前环境不支持复制!");
-    return false;
-  }
-  try {
-    await navigator.clipboard.writeText(text.toString());
-    return true;
-  } catch (err) {
-    useConsoleWarn(`useClipboard: ${err}`);
-    return false;
-  }
+export const useClipboard = (text: string | number) => {
+  const copyBtn = document.createElement("button");
+  copyBtn.setAttribute("data-clipboard-text", text.toString());
+  document.appendChild(copyBtn);
+
+  return new Promise((resolve, reject) => {
+    const clipboard = new ClipboardJS(copyBtn);
+    clipboard.on("success", (e) => {
+      resolve(e);
+    });
+    clipboard.on("error", (e) => {
+      reject(e);
+    });
+    copyBtn.click;
+    copyBtn.remove;
+  });
 };
