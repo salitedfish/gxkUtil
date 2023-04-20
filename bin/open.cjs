be465a5d3862c9390a27407e24073a88b7234604
codergxk <1525185228@qq.com>
2023-04-20 18:17:34 +0800

    feat: update

--- bin/open.cjs
+++ bin/open.cjs
@@ -5,37 +5,37 @@ const open = (argvs) => {
     const name = argvs[0];
     let _name = "";
     if (["weixin", "wx", "WX", "Weixin", "WeiXin"].includes(name)) {
-      _name = "Wechat";
+      _name = "Wechat.app";
     }
     if (["QQ", "qq", "Qq"].includes(name)) {
       _name = "QQ";
     }
     if (["vscode", "VSC", "vsc", "code"].includes(name)) {
-      _name = "Visual Studio Code";
+      _name = "'Visual Studio Code.app'";
     }
     if (["google", "GC"].includes(name)) {
-      _name = "Google Chrome";
+      _name = "'Google Chrome.app'";
     }
     if (["edge", "MSE", "MSC"].includes(name)) {
-      _name = "Microsoft Edge";
+      _name = "'Microsoft Edge.app'";
     }
     if (["snipaste", "snp"].includes(name)) {
-      _name = "Snipaste";
+      _name = "Snipaste.app";
     }
     if (["NM", "nm", "wyymusic"].includes(name)) {
-      _name = "NeteaseMusic";
+      _name = "NeteaseMusic.app";
     }
     if (["QM", "qm", "qqmusic"].includes(name)) {
-      _name = "QQMusic";
+      _name = "QQMusic.app";
     }
     if (["wps", "WPS"].includes(name)) {
-      _name = "wpsoffice";
+      _name = "wpsoffice.app";
     }
     if (["termius", "term"].includes(name)) {
-      _name = "Termius";
+      _name = "Termius.app";
     }
     if (name) {
-      exec("cd /Applications && open " + _name + ".app");
+      exec("cd /Applications && open " + _name);
     }
   } catch (e) {
     console.error(e);
